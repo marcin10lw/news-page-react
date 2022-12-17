0c02ca6391ec6714cc1a785af2f35c91879d1ba6
marcin10lw <marcin10lw@wp.pl>
2022-12-17 19:26:32 +0100

    created Main component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { theme } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
 import { Container } from "./Container/styled";
 import Header from "./Header";
+import { Grid } from "./Grid/styled";
+import Main from "./Main";
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
         <GlobalStyle />
         <Container>
           <Header />
+          <Grid>
+            <Main />
+          </Grid>
         </Container>
       </>
     </ThemeProvider>
---
+++ src/Main/index.js
@@ -0,0 +1,9 @@
+import { StyledMain, Image } from "./styled";
+
+const Main = () => (
+  <StyledMain>
+    <Image src="./assets/images/image-web-3-desktop.jpg"/>
+  </StyledMain>
+)
+
+export default Main;
