3aefe7289c28dd17c047de1bd3ba8c1b63b9fb8c
marcin10lw <marcin10lw@wp.pl>
2022-12-17 20:07:34 +0100

    styled Main

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,15 +1,18 @@
-import { StyledMain, Image, Header, Content, Button } from "./styled";
+import { StyledMain, Image, Heading, Content, Button } from "./styled";
+import { Flex } from "../Flex/styled";
 
 const Main = () => (
   <StyledMain>
     <Image src="./assets/images/image-web-3-desktop.jpg" />
-    <Header>The Bright Future of Web 3.0?</Header>
-    <Content>
-      We dive into the next evolution of the web that claims to put the power of
-      the platforms back into the hands of the people. But is it really
-      fulfilling its promise?
-    </Content>
-    <Button>READ MORE</Button>
+    <Heading>The Bright Future of Web 3.0?</Heading>
+    <Flex>
+      <Content>
+        We dive into the next evolution of the web that claims to put the power
+        of the platforms back into the hands of the people. But is it really
+        fulfilling its promise?
+      </Content>
+      <Button>READ MORE</Button>
+    </Flex>
   </StyledMain>
 );
 
--- src/Main/styled.js
+++ src/Main/styled.js
@@ -4,23 +4,39 @@ export const StyledMain = styled.main`
   max-width: 900px;
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(2, auto);
+  grid-template-columns: repeat(2, 1fr);
+  gap: 30px;
 `;
 
 export const Image = styled.img`
-  width: 100%;
+  max-width: 100%;
   grid-column: span 2;
 `;
 
-export const Header = styled.h1`
+export const Heading = styled.h1`
   font-weight: 800;
+  font-size: 64px;
+  margin: 0;
 `
 
 export const Content = styled.p`
   color: ${({theme}) => theme.colors.darkGrayishBlue};
+  margin: 0;
+  line-height: 1.6;
+  font-size: 18px;
 `
 export const Button = styled.button`
   border: none;
   color: ${({theme}) => theme.colors.offWhite};
   background-color: ${({theme}) => theme.colors.softRed};
+  letter-spacing: 3px;
+  font-size: 20px;
+  padding: 16px 40px;
+  margin-top: 40px;
+  cursor: pointer;
+  transition: background-color 200ms;
+
+  &:hover {
+    background-color: ${({theme}) => theme.colors.veryDarkBlue};
+  }
 `
