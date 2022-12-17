52180377f0340d1eef2403316b27e19f4779a90c
marcin10lw <marcin10lw@wp.pl>
2022-12-17 21:46:02 +0100

    styled Sidebar

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,12 +1,17 @@
 import { StyledSidebar, Heading } from "./styled";
 import { Flex } from "../Flex/styled";
+import Article from "../Article";
 
-const Sidebar = () => (
-  <StyledSidebar>
-    <Flex>
-      <Heading>New</Heading>
-    </Flex>
-  </StyledSidebar>
-);
+const Sidebar = ({ articles }) => {
+  const newArticles = articles.filter(article => article.new);
+  console.log(newArticles);
+  
+  return (
+    <StyledSidebar>
+        <Heading>New</Heading>
+        {newArticles.map(article => <Article title={article.title} content={article.content} />)}
+    </StyledSidebar>
+  );
+};
 
 export default Sidebar;
--- src/Sidebar/styled.js
+++ src/Sidebar/styled.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
-export const StyledSidebar = styled.aside``;
+export const StyledSidebar = styled.aside`
+  background-color: ${({ theme }) => theme.colors.veryDarkBlue};
+  padding: 35px 30px;
+`;
 
 export const Heading = styled.h2`
   color: ${({ theme }) => theme.colors.softOrange};
+  margin: 0;
+  font-size: 42px;
 `;
