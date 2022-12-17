447ca1c568aa8fc6cf71a2914b92392ed76b4aef
marcin10lw <marcin10lw@wp.pl>
2022-12-17 18:58:09 +0100

    created and styled Nav component

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,17 @@
-import { Logo, StyledHeader } from "./styled";
+import { Logo, StyledHeader, Nav } from "./styled";
 
 const Header = () => (
   <StyledHeader>
     <Logo src="./assets/images/logo.svg" />
+    <Nav>
+      <ul>
+        <li><a>Home</a></li>
+        <li><a>New</a></li>
+        <li><a>Popular</a></li>
+        <li><a>Trending</a></li>
+        <li><a>Categories</a></li>
+      </ul>
+    </Nav>
   </StyledHeader>
 );
 
--- src/Header/styled.js
+++ src/Header/styled.js
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-  padding: 40px 0;
+  padding: 80px 40px 60px;
+  border: 1px solid red;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const Logo = styled.img``;
+
+export const Nav = styled.nav`
+  ul {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+
+  a {
+    color: grey;
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.80;
+    }
+  }
+`;
