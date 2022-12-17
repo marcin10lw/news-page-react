da9b754a74715b7541777ef65843f0085d71c1a4
marcin10lw <marcin10lw@wp.pl>
2022-12-17 19:46:14 +0100

    created Main's Image, Content, Button components

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,9 +1,16 @@
-import { StyledMain, Image } from "./styled";
+import { StyledMain, Image, Header, Content, Button } from "./styled";
 
 const Main = () => (
   <StyledMain>
-    <Image src="./assets/images/image-web-3-desktop.jpg"/>
+    <Image src="./assets/images/image-web-3-desktop.jpg" />
+    <Header>The Bright Future of Web 3.0?</Header>
+    <Content>
+      We dive into the next evolution of the web that claims to put the power of
+      the platforms back into the hands of the people. But is it really
+      fulfilling its promise?
+    </Content>
+    <Button>READ MORE</Button>
   </StyledMain>
-)
+);
 
 export default Main;
--- src/Main/styled.js
+++ src/Main/styled.js
@@ -1,5 +1,26 @@
 import styled from "styled-components";
 
-export const StyledMain = styled.main``;
+export const StyledMain = styled.main`
+  max-width: 900px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+`;
 
-export const Image = styled.img``;
+export const Image = styled.img`
+  width: 100%;
+  grid-column: span 2;
+`;
+
+export const Header = styled.h1`
+  font-weight: 800;
+`
+
+export const Content = styled.p`
+  color: ${({theme}) => theme.colors.darkGrayishBlue};
+`
+export const Button = styled.button`
+  border: none;
+  color: ${({theme}) => theme.colors.offWhite};
+  background-color: ${({theme}) => theme.colors.softRed};
+`
