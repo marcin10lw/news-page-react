fd12b67cab1e4b2e35349e255b3fc3b239d0ceef
marcin10lw <marcin10lw@wp.pl>
2022-12-17 20:22:36 +0100

    created Sidebar component

---
+++ src/Sidebar/index.js
@@ -0,0 +1,9 @@
+import { StyledSidebar, Heading } from "./styled";
+
+const Sidebar = () => (
+  <StyledSidebar>
+    <Heading>New</Heading>
+  </StyledSidebar>
+);
+
+export default Sidebar;
---
+++ src/Sidebar/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledSidebar = styled.aside`
+  max-width: 500px;
+  width: 100%;
+`;
+
+export const Heading = styled.h2`
+  color: ${({theme}) => theme.colors.softOrange};
+`
