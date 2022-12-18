827278054d358c1fdbfc44e639640d41e8ee36f4
marcin10lw <marcin10lw@wp.pl>
2022-12-18 11:32:08 +0100

    added media queries to Grid

--- src/Grid/styled.js
+++ src/Grid/styled.js
@@ -4,4 +4,9 @@ export const Grid = styled.div`
   display: grid;
   grid-template-columns: 2fr 1fr;
   gap: 30px;
+
+  @media (max-width: ${({theme}) => theme.mobile}px) {
+    grid-template-columns: none;
+    grid-template-rows: repeat(2, 1fr);
+  }
 `
