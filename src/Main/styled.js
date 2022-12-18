d8e531b9d41a47a9ba54987275cd49564b9ba862
marcin10lw <marcin10lw@wp.pl>
2022-12-18 11:31:55 +0100

    added media queries to mobile

--- src/Main/styled.js
+++ src/Main/styled.js
@@ -1,32 +1,57 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const StyledMain = styled.main`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 30px;
+
+  @media (max-width: ${({ theme }) => theme.mobile}px) {
+    grid-template-columns: none;
+    grid-template-rows: repeat(3, auto);
+  }
 `;
 
 export const Image = styled.img`
   max-width: 100%;
   grid-column: span 2;
+
+  @media (max-width: ${({ theme }) => theme.mobile}px) {
+    grid-column: 1;
+  }
+
+  ${({ desktop }) =>
+    desktop &&
+    css`
+      @media (max-width: ${({ theme }) => theme.mobile}px) {
+        display: none;
+      }
+    `}
+
+  ${({ mobile }) =>
+    mobile &&
+    css`
+      @media (min-width: ${({ theme }) => theme.mobile}px) {
+        display: none;
+      }
+    `}
 `;
 
 export const Heading = styled.h1`
   font-weight: 800;
   font-size: 64px;
   margin: 0;
-`
+`;
 
 export const Content = styled.p`
-  color: ${({theme}) => theme.colors.darkGrayishBlue};
+  color: ${({ theme }) => theme.colors.darkGrayishBlue};
   margin: 0;
   line-height: 1.6;
   font-size: 18px;
-`
+`;
 export const Button = styled.button`
   border: none;
-  color: ${({theme}) => theme.colors.offWhite};
-  background-color: ${({theme}) => theme.colors.softRed};
+  color: ${({ theme }) => theme.colors.offWhite};
+  background-color: ${({ theme }) => theme.colors.softRed};
   letter-spacing: 3px;
   font-size: 20px;
   padding: 16px 40px;
@@ -35,6 +60,6 @@ export const Button = styled.button`
   transition: background-color 200ms;
 
   &:hover {
-    background-color: ${({theme}) => theme.colors.veryDarkBlue};
+    background-color: ${({ theme }) => theme.colors.veryDarkBlue};
   }
-`
+`;
