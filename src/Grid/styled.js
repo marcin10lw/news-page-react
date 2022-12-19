9d8929f4599dd69be3b33caae165296e9c977f39
marcin10lw <marcin10lw@wp.pl>
2022-12-19 15:05:15 +0100

    changed sidebar width in Grid

--- src/Grid/styled.js
+++ src/Grid/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Grid = styled.div`
   display: grid;
-  grid-template-columns: 2fr 1fr;
+  grid-template-columns: 2fr 430px;
   gap: 30px;
 
   @media (max-width: ${({theme}) => theme.mobile}px) {
